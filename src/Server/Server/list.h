f72de3e823ce946390aa78a307c07815ddc8bea7
ableigdev <igor.yarovoy.98@ukr.net>
2018-03-17 21:26:14 +0200

    Added template class TList

---
+++ src/Server/Server/list.h
@@ -0,0 +1,244 @@
+#pragma once
+
+#include <iostream>
+
+template <typename INF, typename LISTTYPE> class Iterator;
+
+// Шаблонный класс для работы с однонаправленным списком
+template <typename INF> class TList
+{
+	friend	class Iterator <INF, TList<INF > >;
+
+	// тип данных, который описывает элемент списка
+	// Информационна¤ часть (полезная нагрузка) + указатель на следующий элемент (когда он равен NULL, то это последний элемент)
+
+	template <typename ElemType> struct TElem
+	{
+		ElemType Inf;
+		TElem *Next;
+
+	};
+
+	typedef TElem<INF> ListElem;
+
+protected:
+
+	// Описываем указатели на первый и последний элементы, которые недоступны пользователю
+	TElem<INF> *Head, *Tail;
+
+public:
+
+	typedef Iterator <INF, TList<INF > > IteratorTList;
+
+	TList()
+	{
+		Head = Tail = NULL;
+	}
+
+	~TList()
+	{
+		DelAllElem();
+	}
+
+	//-----------------------------------------------------------------------
+
+	// Вставка нового элемента после последнего. Элемент задается своей информационной частью
+
+	void AddAfterTail(INF Inf)
+	{
+		ListElem *Cur = new TElem<INF>;
+
+		Cur->Inf = Inf;
+		Cur->Next = NULL;
+		if (Head != NULL)
+			Tail->Next = Cur;
+		else
+			Head = Cur;
+		Tail = Cur;
+	}
+
+	// Вставка нового элемента после последнего. Элемент задается своей информационной частью
+	// Для корректной работы в многопоточной среде применяются interlocked-функции
+
+	void AddAfterTailInterlocked(INF Inf)
+	{
+		ListElem *Cur = new TElem<INF>;
+
+		Cur->Inf = Inf;
+		Cur->Next = NULL;
+		if (Head != NULL)
+			InterlockedExchange((LONG*)(&(Tail->Next)), (LONG)Cur);
+		else
+			InterlockedExchange((LONG*)(&Head), (LONG)Cur);
+
+		InterlockedExchange((LONG*)(&Tail), (LONG)Cur);
+
+	}
+
+
+
+	//-----------------------------------------------------------------------
+
+	// Поиск элемента. Элемент задается своей информационной частью
+
+	bool SearchElement(INF Inf, IteratorTList &It)
+	{
+		for (It = *this; !It && !(It == Inf); ++It);
+		return It.GetCurInf(Inf);
+
+	}
+
+	//-----------------------------------------------------------------------
+
+	// Проверка существования элементов в списке
+
+	bool IsNotEmpty()
+	{
+		return (Head != NULL);
+	}
+
+	//-----------------------------------------------------------------------
+
+	// Удаление всех элементов из списка
+
+	void DelAllElem()
+	{
+		ListElem *Cur = Head;
+
+		while (Head != NULL)
+		{
+			Cur = Head;
+			Head = Head->Next;
+			delete Cur;
+		}
+
+		Tail = NULL;
+	}
+
+	friend ostream& operator << (ostream &os, TList &List);
+
+};
+
+
+//-----------------------------------------------------------------------
+
+
+template <typename INF> ostream& operator << (ostream &os, TList<INF> &List)
+{
+	INF *Val = NULL;
+
+	for (TList<INF>::IteratorTList It = List; !It; ++It)
+	{
+		It.GetCurInf(Val);
+		os << *Val << endl;
+	}
+
+	return os;
+}
+
+//-----------------------------------------------------------------------
+
+template <typename INF, class LISTTYPE> class Iterator
+{
+	LISTTYPE::ListElem **Head, **Tail, *CurPos;
+
+public:
+
+	Iterator()
+	{
+		Head = Tail = CurPos = NULL;
+	}
+
+	Iterator(Iterator &It)
+	{
+		*this = It;
+	}
+
+	Iterator(LISTTYPE &List)
+	{
+		*this = List;
+	}
+
+	//---------------------------------------------------
+
+	Iterator& operator = (Iterator &It)
+	{
+		Head = It.Head;
+		Tail = It.Tail;
+		CurPos = It.CurPos;
+		return *this;
+	}
+
+	Iterator& operator = (LISTTYPE &List)
+	{
+		Head = &(List.Head);
+		Tail = &(List.Tail);
+		CurPos = List.Head;
+		return *this;
+	}
+
+	Iterator& operator = (INF &Val)
+	{
+		if (CurPos)
+			CurPos->Inf = Val;
+
+		return *this;
+	}
+
+	//---------------------------------------------------
+
+	Iterator& operator ++()
+	{
+		if (CurPos)
+			CurPos = CurPos->Next;
+
+		return *this;
+	}
+
+	//---------------------------------------------------
+
+	bool operator !()
+	{
+		return CurPos != NULL;
+	}
+
+	bool operator == (INF Val)
+	{
+		return CurPos ? CurPos->Inf == Val : false;
+	}
+
+	//---------------------------------------------------
+
+	void SetStart()
+	{
+		CurPos = *Head;
+	}
+
+	bool IsNotEmpty()
+	{
+		return (*Head != NULL);
+	}
+
+	//---------------------------------------------------
+
+	bool GetCurInf(INF &Inf)
+	{
+		if (CurPos)
+		{
+			Inf = CurPos->Inf;
+			return true;
+		}
+		return false;
+	}
+
+	bool GetCurInf(INF *&Inf)
+	{
+		if (CurPos)
+		{
+			Inf = &(CurPos->Inf);
+			return true;
+		}
+		return false;
+	}
+
+};
